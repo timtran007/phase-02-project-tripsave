6c595526e1cda3919399311c3f07388d70308ba9
Tim <timtran007@gmail.com>
2022-11-04 23:54:47 -0500

    imported Route in App component & added Route to DestinationContainer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import {Route} from "react-router-dom"
 import Nav from "./Nav"
+import DestinationContainer from "./DestinationContainer";
 
 function App() {
   return (
     <div>
       <Nav />
+      <Route path="/destination">
+        <DestinationContainer />
+      </Route>
     </div>
   );
 }
