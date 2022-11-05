3829f77d236864445c25b16bcec89c8b3d1296a3
Tim <timtran007@gmail.com>
2022-11-04 23:01:53 -0500

    set up DestinationContainer contains DestinationImage & DestinationInfo

---
+++ src/DestinationContainer.js
@@ -0,0 +1,13 @@
+import DestinationImage from "./DestinationImage"
+import DestinationInfo from "./DestinationInfo"
+
+function DestinationContainer(){
+    return(
+        <div>
+            <DestinationImage />
+            <DestinationInfo />
+        </div>
+    )
+}
+
+export default DestinationContainer
