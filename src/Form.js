fb507257e28eb6a074a14d23829494cc7afdcd7f
Tim <timtran007@gmail.com>
2022-11-05 11:06:06 -0500

    added form inputs and title for Form component

--- src/Form.js
+++ src/Form.js
@@ -2,7 +2,17 @@
 function Form(){
     return(
         <div>
+            <div><h3>Add My Dream Trip</h3></div>
             
+            <form>
+                <input type="text" name="destination" placeholder="enter a destination"></input>
+                <input type="text" name="imageURL" placeholder="add a image URL link"></input>
+                <input type="number" name="budget" placeholder="enter a budget">
+                </input>
+                <input type="number" name="savings" placeholder="enter savings amount">
+                </input>
+                <input type="submit" value="Add a Trip"></input>
+            </form>
         </div>
     )
 }
