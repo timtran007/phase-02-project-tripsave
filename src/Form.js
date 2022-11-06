507f42cc32433955e9e826ac369de8cf3b015d73
Tim <timtran007@gmail.com>
2022-11-05 22:14:42 -0500

    formatted Form component to be more readable

--- src/Form.js
+++ src/Form.js
@@ -1,19 +1,51 @@
 
 function Form({formData}){
     function handleFormData(event){
+        console.log(formData)
         console.log(event.target.value)
     }
     return(
         <div>
-            <div><h3>Add My Dream Trip</h3></div>
+            <div><h2>Add My Dream Trip</h2></div>
             <form>
-                <input onChange={handleFormData} type="text" name="destination" placeholder="enter a destination"></input>
-                <input onChange={handleFormData} type="text" name="imageURL" placeholder="add a image URL link"></input>
-                <input onChange={handleFormData} type="number" name="budget" placeholder="enter a budget">
-                </input>
-                <input onChange={handleFormData} type="number" name="savings" placeholder="enter savings amount">
-                </input>
-                <input type="submit" value="Add a Trip"></input>
+                <p>
+                    <label htmlFor="destination">Destination:  </label>
+                        <input 
+                            onChange={handleFormData} 
+                            type="text" 
+                            name="destination" 
+                            placeholder="enter a destination"
+                        />
+                </p>
+                <p>
+                    <label htmlFor="imageURL">Image URL:  </label>
+                        <input 
+                            onChange={handleFormData} 
+                            type="text" name="imageURL" 
+                            placeholder="add a image URL link"
+                        />
+                </p>
+                <p>
+                    <label htmlFor="budget">Budget:  </label>
+                        <input 
+                            onChange={handleFormData} 
+                            type="number" 
+                            name="budget" 
+                            placeholder="enter a budget"
+                        />
+                </p> 
+                <p>
+                    <label htmlFor="savings">Savings:  </label>
+                        <input 
+                            onChange={handleFormData} 
+                            type="number" 
+                            name="savings"
+                            placeholder="enter savings amount"
+                        />
+                </p>
+                <p>
+                    <input type="submit" value="Add a Trip"/>
+                </p>
             </form>
         </div>
     )
