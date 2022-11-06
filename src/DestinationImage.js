4b7d899127d90e364cfaeb4b1f1c6ef913ffa96b
Tim <timtran007@gmail.com>
2022-11-06 00:38:27 -0500

    rendered image in DestinationImage component using map, assign key and alt attributes

--- src/DestinationImage.js
+++ src/DestinationImage.js
@@ -1,11 +1,14 @@
 
 function DestinationImage({tripData}){
     const imageToDisplay = tripData.map(data =>{
-        console.log(data)
+        return(
+            <img key={data.id} src={data.imgURL} alt={data.name}/>
+        )
     })
     return(
         <div>
-
+            <h2>My Trips</h2>
+            {imageToDisplay}
         </div>
     )
 }
