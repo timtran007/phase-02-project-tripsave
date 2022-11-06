03e402960260acf69b1ce13e48c19090ef4fed12
Tim <timtran007@gmail.com>
2022-11-05 21:55:16 -0500

    added event listener to input fields in Form component

--- src/Form.js
+++ src/Form.js
@@ -1,15 +1,17 @@
 
 function Form(){
+    function handleFormData(event){
+        console.log(event.target.value)
+    }
     return(
         <div>
             <div><h3>Add My Dream Trip</h3></div>
-            
             <form>
-                <input type="text" name="destination" placeholder="enter a destination"></input>
-                <input type="text" name="imageURL" placeholder="add a image URL link"></input>
-                <input type="number" name="budget" placeholder="enter a budget">
+                <input onChange={handleFormData} type="text" name="destination" placeholder="enter a destination"></input>
+                <input onChange={handleFormData} type="text" name="imageURL" placeholder="add a image URL link"></input>
+                <input onChange={handleFormData} type="number" name="budget" placeholder="enter a budget">
                 </input>
-                <input type="number" name="savings" placeholder="enter savings amount">
+                <input onChange={handleFormData} type="number" name="savings" placeholder="enter savings amount">
                 </input>
                 <input type="submit" value="Add a Trip"></input>
             </form>
