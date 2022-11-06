d7856ebe70917590f8d5fa6e1676ab644b395db3
Tim <timtran007@gmail.com>
2022-11-06 02:06:29 -0600

    removed state of data, associated props, and import of db

--- src/DestinationContainer.js
+++ src/DestinationContainer.js
@@ -1,14 +1,12 @@
-import React, {useState} from "react"
 import DestinationInfo from "./DestinationInfo"
-import db from "../src/data/db.json"
+
 
 function DestinationContainer(){
-    const [data, setData] = useState(db.destination)
 
     return(
         <div>
             <h2>My Trips</h2>
-            <DestinationInfo tripData={data}/>
+            <DestinationInfo />
         </div>
     )
 }
