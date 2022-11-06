44aa5f68f1b6b1e293802f1247c6d00675a55bce
Tim <timtran007@gmail.com>
2022-11-06 01:42:42 -0500

    added prop for setTripData to send down to AddFunds

--- src/DestinationContainer.js
+++ src/DestinationContainer.js
@@ -3,12 +3,12 @@ import DestinationInfo from "./DestinationInfo"
 import db from "../src/data/db.json"
 
 function DestinationContainer(){
-    const [data, setData] = useState(db)
+    const [data, setData] = useState(db.destination)
 
     return(
         <div>
             <h2>My Trips</h2>
-            <DestinationInfo tripData={data.destination}/>
+            <DestinationInfo tripData={data} setTripData={setData}/>
         </div>
     )
 }
