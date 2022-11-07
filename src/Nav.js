ec8b63b1cacf54f83981d098d6d477c15729358d
Tim <timtran007@gmail.com>
2022-11-06 23:13:24 -0600

    added styling to Nav component through Nav.css

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,17 @@
 import React from "react"
 import {NavLink} from "react-router-dom"
+import Navbar from "../src/styles/Navbar.css"
 
 function Nav(){
     return(
         <nav >
-                <NavLink activeClassName="active" to="/">
+                <NavLink exact className="nav" activeClassName="active" to="/">
                     Home
                 </NavLink>
-                <NavLink activeClassName="active" to="/destination">
+                <NavLink className="nav" to="/destination">
                     Trips
                 </NavLink> 
-                <NavLink activeClassName="active" to="add-a-trip">
+                <NavLink className="nav" to="add-a-trip">
                     Add a Trip
                 </NavLink>
         </nav>
